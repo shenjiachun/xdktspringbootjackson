be931019c48ee7148b8a818da4d8b2873e3a0be8
xuixiaoxiao <xuxiaoxiao@users.noreply.github.com>
2018-05-11 22:11:11 +0800

    jackson简单小案例

---
+++ springbootstudy/src/main/java/com/jincou/SpringbootstudyApplication.java
@@ -0,0 +1,12 @@
+package com.jincou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstudyApplication.class, args);
+	}
+}
