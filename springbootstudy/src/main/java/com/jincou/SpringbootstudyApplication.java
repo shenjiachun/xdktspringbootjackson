168ed003538500ccceba31d376ea8ef40270b850
xuixiaoxiao <xuxiaoxiao@users.noreply.github.com>
2018-05-15 23:03:54 +0800

    springboot第二篇文章到第六篇文章拦截器为止额所有代码

--- springbootstudy/src/main/java/com/jincou/SpringbootstudyApplication.java
+++ springbootstudy/src/main/java/com/jincou/SpringbootstudyApplication.java
@@ -2,8 +2,10 @@ package com.jincou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringbootstudyApplication {
 
 	public static void main(String[] args) {
